b0cad2ce813e96a735579f6041d64d33e91ad4bb
kiss81 <djsavale@hotmail.com>
2020-04-13 20:35:47 +0200

    several updates

--- install.sh
+++ install.sh
@@ -2,14 +2,21 @@
 
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
+sudo apt update;
 # core install
-sudo apt update; sudo apt -y install screen htop remmina remmina-plugin-vnc firefox build-essential git dwm suckless-tools dmenu xorg virt-viewer conky
+sudo apt -y install build-essential git dwm suckless-tools xorg conky acpi-support alsa-utils pulseaudio lm-sensors curl wget htop
+#extra install 
+sudo apt -y install screen remmina remmina-plugin-vnc firefox virt-viewer numix-gtk-theme thunar papirus-icon-theme smbclient tumbler tumbler-plugins-extra ffmpegthumbnailer
+
 
 # core settup stuff
 cp xsession ~/.xsession
 mkdir ~/.config
 mkdir ~/.config/conky
 cp conky.conf ~/.config/conky/conky.conf
+mkdir ~/.config/gtk-3.0/
+cp settings.ini ~/.config/gtk-3.0/
+cp gtkrc-2.0 ~/.gtkrc-2.0
 
 #autostartx without xdm
 sudo mkdir /etc/systemd/system/getty@tty1.service.d/
@@ -25,7 +32,7 @@ sudo cp rc-local.service /etc/systemd/system/
 sudo systemctl enable rc-local
 
 # office
-sudo apt -y install libreoffice aspell-nl
+sudo apt -y install libreoffice aspell-nl cups
 
 # multimedia
 sudo apt -y install mpv youtube-dl celluloid
@@ -33,6 +40,10 @@ sudo apt -y install mpv youtube-dl celluloid
 #redshift
 sudo apt -y install redshift-gtk
 cp redshift.conf ~/.config/
+
+#dpi and wallpaper
+cp wallpaper.pgn ~/
+cp Xresources ~/.Xresources
 
 # laptop tools
 sudo apt -y install tlp-rdw powertop
@@ -47,3 +58,12 @@ sudo make install
 
 cd $SCRIPTPATH
 sudo cp intel-undervolt.conf /etc/intel-undervolt.conf
+
+#development
+sudo usermod -aG dialout $USER
+
+
+# todo 
+#gtk theme numix as default?
+#network tools
+#fix  hardcoded login sander to $USER
